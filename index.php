b2e6ec67840bd44a0c58eb553cc2ca20068b260d
Priyanka Singh <71539429+priyankars669@users.noreply.github.com>
2021-01-20 22:27:48 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Priyanka Singh PHP</title>
+</head>
+<body>
+	<h1>Priyanka Singh PHP</title>
+	<p>ASCII ART:</p>
+	<pre>
+    ******
+    **    **
+    **    **
+    *******
+    **
+    **
+    **
+	</pre>
+	<p>
+		The SHA256 hash of "Priyanka Singh" is <?php print hash('sha256', 'Priyanka Singh'); ?>
+	</p>
+</body>
+</html>
+	
